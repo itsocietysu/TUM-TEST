d12e54ce397d162b16f218d4a109803f4f1f3578
Jannyss <jannyss@mail.ru>
2018-09-17 06:58:28 -1200

    Update admin.py

--- jedi_academy/admin.py
+++ jedi_academy/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Planet, Jedi, Question
 
-# Register your models here.
+admin.site.register(Planet)
+admin.site.register(Jedi)
+admin.site.register(Question)
