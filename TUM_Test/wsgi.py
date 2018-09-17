1b8c852e6821ef287b416104c78156adaada8eff
Igor Khalepskiy <ikhalepsky@gmail.com>
2018-09-17 14:58:05 +0300

    Update wsgi.py

--- TUM_Test/wsgi.py
+++ TUM_Test/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TUM_test project.
+WSGI config for TUM_Test project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TUM_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TUM_Test.settings")
 
 application = get_wsgi_application()
